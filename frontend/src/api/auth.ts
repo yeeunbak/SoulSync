2425edf37ab932801ece6c83af625ccba0af0d4c
woojoong <7774vv@naver.com>
2025-05-27 18:01:49 +0900

    임시 로그인 구현

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 export async function login(username: string, password: string) {
-  const response = await axios.post('http://localhost:8000/user/login', {
+  return await axios.post('http://127.0.0.1:8000/user/login', {
     username,
     password,
   });
-  return response;
 }
